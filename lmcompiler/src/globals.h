99899a7e5de8b0cdf37539578c2ee8a7e7296c8b
Tom <admin@tomj.xyz>
2023-01-26 21:03:58 +0000

    Added argument parsing Fixed not generating debug symbols with makefile

---
+++ lmcompiler/src/globals.h
@@ -0,0 +1,14 @@
+#include <argp.h>
+
+const char *argp_program_version = "lmcomp 0.1";
+const char *argp_program_bug_address = "<admin@tomj.xyz>";
+
+static char doc[] = 
+	"LetterMark CLI Compiler\vReplace INPUT_FILE with - to read from STDIN";
+static char args_doc[] = "INPUT_FILE OUTPUT_FILE";
+
+static struct argp_option options[] = {
+    {"config", 'c', "FILE", 0, "Explicit configuration file path"},
+    {"quiet", 'q', 0, 0, "Suppress logging to STDOUT"},
+    {0}
+};
--- lmcompiler/src/main.c
+++ lmcompiler/src/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <argp.h>
 
-int main()
+#include "globals.h"
+
+typedef struct arguments
 {
-	printf("Hello World\n");
+	char *np_args[2];
+	char *config_file;
+	int quiet;
+} t_args;
+
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	t_args *input = state->input;
+
+	switch (key)
+	{
+		case 'c':
+			input->config_file = arg;
+			break;
+		case 'q':
+			input->quiet = 1;
+			break;
+		case ARGP_KEY_ARG:	// Non-option argument
+			if (state->arg_num >=2)
+				argp_usage(state);	// Too many arguments
+			input->np_args[state->arg_num] = arg;	// Sets the input strings
+			break;
+		case ARGP_KEY_END:
+			if (state->arg_num < 2)
+				argp_usage(state);	// Not enough arguments
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+
+	return 0;
+}
+
+static struct argp argparse = {options, parse_opt, args_doc, doc};
+
+int main(int argc, char **argv)
+{
+	// Default state
+	t_args args;
+	args.quiet = 0;
+	args.config_file = NULL;
+
+	argp_parse(&argparse, argc, argv, 0, 0, &args);
+
 	return 0;
 }

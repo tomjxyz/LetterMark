94a62f53b2f143b4f84d4d7277de4f6b02e008c0
Tom <admin@tomj.xyz>
2023-03-16 16:10:44 +0000

    Added all features except UL and OL to JS preview

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -5,6 +5,7 @@ let ta, oa;
 document.addEventListener('DOMContentLoaded', function (e) {
     ta = document.querySelector('textarea');
     oa = document.querySelector('.render');
+    render();
 });
 
 // Handler for each keypress
@@ -14,25 +15,58 @@ function render() {
     for (line of source)
     {
         let plain = false;
-        let elm;
+        // let inline = false;
+        // console.log(line.slice(-1));
+        // if (line.slice(-1) === '\\' && line.slice(-2) !== '\\\\')
+        // {
+        //     line = line.substring(0, line.length - 1);
+        //     inline = true;
+        //     prevInline = true;
+        // }
+        // else if (line.slice(-2) === '\\\\')
+        // {
+        //     line = line.substring(0, line.length - 1);
+        // }
+
+
+        let elm, text;
+        text = line.substring(3);
         switch (line.substring(0, 3)) {
             case '.b ':
                 elm = document.createElement('b');
-                elm.innerText = line.substring(3);
+                elm.innerText = text;
                 break;
             case '.i ':
                 elm = document.createElement('i');
-                elm.innerText = line.substring(3);
+                elm.innerText = text;
                 break;
             case '.h ':
                 elm = document.createElement('h1');
-                elm.innerText = line.substring(3);
+                elm.innerText = text;
                 break;
             case '.s ':
                 elm = document.createElement('h3');
-                elm.innerText = line.substring(3);
+                elm.innerText = text;
                 break;
-        
+            case '.a ':
+                elm = document.createElement('a');
+                elm.innerText = text;
+                elm.href = text;
+                break;
+            case '.m ':
+                elm = document.createElement('img');
+                elm.src = text;
+                break;
+            case '.c ':
+                elm = document.createElement('span');
+                elm.style.fontFamily = 'monospace';
+                elm.innerText = text;
+                break;
+            case '.q ':
+                elm = document.createElement('i');
+                elm.innerText = `   "${text}"`;
+                break;
+
             default:
                 plain = true;
                 break;
@@ -49,6 +83,7 @@ function render() {
                 elm.innerText = line;
             }
         }
+        // if (inline) {elm.style.display = 'inline';}
         oa.append(elm);
     }
 }
